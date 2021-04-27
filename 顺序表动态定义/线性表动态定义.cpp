b319f357fa6627e87643b74e36f6773eba11bb4d
mEm0ry <380080610@qq.com>
2021-04-27 17:57:55 +0800

    顺序表动态定义

---
+++ 顺序表动态定义/线性表动态定义.cpp
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+#include <stdlib.h>
+#define InitSize 10
+
+typedef struct {                                                    //定义动态分配的顺序表
+	int *data;
+	int MaxSize;
+	int length;
+}SeqList;
+
+void InitList(SeqList &L) {                                        //初始化
+	L.data = (int*)malloc(InitSize * sizeof(int));
+	L.length = 0;
+	L.MaxSize = InitSize;
+
+}
+void IncreaseSize(SeqList &L, int len) {                          //增加顺序表长度
+	int *p = L.data;                                              
+	L.data = (int*)malloc((L.MaxSize + len) * sizeof(int));
+	for (int i = 0; i < L.length; i++) {
+		L.data[i] = p[i];
+	}
+	L.MaxSize = L.MaxSize + len;
+	free(p);
+
+}
+int main() {
+
+	SeqList L;
+	InitList(L);
+	IncreaseSize(L, 3);
+
+
+	return 0;
+}
